de516ea0a1de926a5c01dda8e5e123d59eb8dfa2
Roderick Lan <rodericklan03@gmail.com>
2025-01-10 16:36:55 -0700

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
+#include <cmath>
 #include <iostream>
+#include <vector>
+#include "constants.hpp"
 #include "engine.hpp"
 #include "utils.hpp"
+#include "xtensor/xrandom.hpp"
+#include "xtensor/xtensor_forward.hpp"
 
 int main() {
-    std::cout << "done\n";
-    return 0;
+    cnn::Perceptron net{10};
+    net.addLayer(Activation::none);
+
+    std::vector<size_t> s{2,2};
+    xt::xarray<int> a{s};
+
+    xt::random::seed(CONSTANT::SEED);
+    // xt::random::rand()
+    double bound = 1 / std::sqrt(4);
+    a = xt::random::rand(
+        a.shape(),
+        -bound,
+        bound
+    );
+
+    std::cout << a << std::endl;
 }
