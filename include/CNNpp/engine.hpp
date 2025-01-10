f7829472907ea68922d46878ee948e64a431f203
Roderick Lan <rodericklan03@gmail.com>
2025-01-10 13:27:07 -0700

    started activations

--- include/CNNpp/engine.hpp
+++ include/CNNpp/engine.hpp
@@ -1,20 +1,35 @@
 #ifndef ENGINE_HPP_
 #define ENGINE_HPP_
 
-#include "xtensor/xtensor_forward.hpp"
+// #include "xtensor/xtensor_forward.hpp"
+#include "utils.hpp"
 #include <iostream>
+#include <memory>
 #include <xtensor.hpp>
 #include <functional>
+#include <vector>
 
 
 namespace cnn {
     class Layer {
     public:
         Layer() = default;
+        Layer(size_t in_dim, size_t out_dim, std::function<double(float)> activation) 
+        : in_dim{in_dim}, out_dim{out_dim}, activation{activation} {
+            // TODO set backprop
+            std::cout << "\nALERT: backprop not set\n";
+        }
+        
+        Layer(size_t in_dim, size_t out_dim, Activation activation) 
+        : in_dim{in_dim}, out_dim{out_dim} {
+            this->activation = actutils::activation.at(activation);
+            this->backprop = actutils::backprop.at(activation);
+        }
 
     private:
-        size_t in_dim, out_dim;
-        std::function<double()> backprop;
+        size_t in_dim, out_dim;                         // input and output dimensions
+        std::function<double(float)> activation;        // activation functor
+        std::function<double(float)> backprop;          // backprop functor
         
         // weight matrix
         // TODO: change to tensor or tensor_fixed (if possible)
@@ -23,7 +38,25 @@ namespace cnn {
 
 
     class CNN {
+    public:
+        CNN() : n_layers{}, layers{}, features{} {};
+
+        void addLayer(std::function<double(float)> activation) {
+            Layer new_layer{};
+        }
+
+        void addLayer(Activation activation) {
+            Layer new_layer{};
+        }
+
+    private:
+        size_t n_layers{};
+        std::vector<Layer> layers;
         
+        // matrix of n features for m samples (m x n) (r x c)
+        // TODO: dont use ptr
+        std::vector<std::unique_ptr<xt::xarray<float>>> features;
+
     };
 }
 #endif
---
+++ include/CNNpp/utils.hpp
@@ -0,0 +1,94 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+#include <functional>
+#include <unordered_map>
+#include <cmath>
+#include <iostream>
+
+
+enum class Activation {
+    none, // default (linear)
+    sigmoid, relu, tanh
+};
+
+
+// TODO: change to structs?
+// Activation functors
+namespace AFunctors {
+    const std::function<double(float)> linear = [](float val) {
+        return val;
+    };
+
+    const std::function<double(float)> sigmoid = [](float val) {
+        double num, denom;
+        if (val < 0) {
+            num = std::exp(val);
+            denom = 1 + std::exp(val);
+        } else {
+            num = 1;
+            denom = 1 + std::exp(-val);
+
+        }
+        return num / denom;
+    };
+
+    const std::function<double(float)> relu = [](float val) {
+        if (val <= 0) {
+            return 0.0;
+        } else {
+            return static_cast<double>(val);
+        }
+    };
+    
+    // TODO: finish tanh
+    const std::function<double(float)> tanh = [](float val) {
+        std::cout << "\nALERT: TANH FUNCTION UNDEFINED\n";
+        return 0;
+    };
+}
+
+// TODO: finish backprop functions
+// backprop functors
+namespace BFunctors {
+    const std::function<double(float)> linear = [](float val) {
+        std::cout << "\nALERT: LINEAR BACKPROP/GRADIENT FUNCTION UNDEFINED\n";
+        return 0;
+    };
+
+    const std::function<double(float)> sigmoid = [](float val) {
+        std::cout << "\nALERT: SIGMOID BACKPROP/GRADIENT FUNCTION UNDEFINED\n";
+        return 0;
+    };
+
+    const std::function<double(float)> relu = [](float val) {
+        std::cout << "\nALERT: RELU BACKPROP/GRADIENT FUNCTION UNDEFINED\n";
+        return 0;
+    };
+
+    const std::function<double(float)> tanh = [](float val) {
+        std::cout << "\nALERT: TANH BACKPROP/GRADIENT FUNCTION UNDEFINED\n";
+        return 0;
+    };
+}
+
+
+// maps
+namespace actutils {
+    const std::unordered_map<Activation, std::function<double(float)>> activation{
+        {Activation::none, AFunctors::linear},
+        {Activation::relu, AFunctors::relu},
+        {Activation::sigmoid, AFunctors::sigmoid},
+        {Activation::tanh, AFunctors::tanh},
+    };
+
+    const std::unordered_map<Activation, std::function<double(float)>> backprop{
+        {Activation::none, BFunctors::linear},
+        {Activation::relu, BFunctors::relu},
+        {Activation::sigmoid, BFunctors::sigmoid},
+        {Activation::tanh, BFunctors::tanh},
+    };
+}
+
+
+
+#endif
